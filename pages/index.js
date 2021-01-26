1e556625f07af8a7b63efbd6d30c2558c81141fe
zarp_lbarros <luiz.barros@zarpsystem.com.br>
2021-01-26 14:49:28 -0300

    primeira parte da segunda aula

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Head from "next/head";
+import { useRouter } from 'next/router';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -19,6 +20,9 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
@@ -30,10 +34,29 @@ export default function Home() {
         <QuizLogo />
         <Widget>
           <Widget.Header>
-            <h1>{db.title}</h1>
+            <h1>The legend of zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(infosDoEvento.target.value);
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz ai seu nome"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
 
