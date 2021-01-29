3d83b22c17a51aa483bf7d617861e4e24e878a55
zarp_lbarros <luiz.barros@zarpsystem.com.br>
2021-01-28 23:04:46 -0300

    alterações da 4 aula

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,9 @@ import Head from "next/head";
 import { useRouter } from 'next/router';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
+import { motion } from 'framer-motion';
+import Link from '../src/components/Link';
+
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -35,6 +38,16 @@ export default function Home() {
       </Head>
       <QuizContainer>
         <QuizLogo />
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        ></Widget>
         <Widget>
           <Widget.Header>
             <h1>{db.title}</h1>
@@ -59,15 +72,53 @@ export default function Home() {
             </form>
           </Widget.Content>
         </Widget>
+        
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
 
-        <Widget>
-          <Widget.Content>
+        <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
-        </Widget>
-        <Footer />
+          </Widget>
+          <Footer
+          as={motion.footer}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/lbarroslgb" />
     </QuizBackground>
--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -1,11 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import db from '../db.json';
-import Widget from '../src/components/Widget';
-import QuizLogo from '../src/components/QuizLogo';
-import QuizBackground from '../src/components/QuizBackground';
-import QuizContainer from '../src/components/QuizContainer';
-import Button from '../src/components/Button';
+import db from '../../db.json';
+import Widget from '../../src/components/Widget';
+import QuizLogo from '../../src/components/QuizLogo';
+import QuizBackground from '../../src/components/QuizBackground';
+import QuizContainer from '../../src/components/QuizContainer';
+import Button from '../../src/components/Button';
 
 function LoadingWidget() {
   return (
